ea17c6ea29a7636a6c116decfa994b5fa91c54e9
Ray Clanan <rclanan@utopianconcept.com>
2014-09-12 22:43:49 -0400

    Add parsing of yaml file

--- run.sh
+++ run.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 if [ -f vagrant_config.yml ]; then
   # Parse config file for vagrantdirectory
-  cd vagrantdirectory
+  eval $(parse_yaml vagrant_config.yml "vagrant_")
+  cd vagrant_directory
   vagrant up
+
+  echo "Vagrant Box ($vagrant_box) is running on http://localhost:$vagrant_port"
 fi
+
+# https://gist.github.com/pkuczynski/8665367
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
